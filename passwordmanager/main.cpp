dbe60ab453c33e0c79c5fefe86555d90f6fed8c3
Arnob <raheearnob@gmail.com>
2023-04-28 21:41:01 -0400

    3rd commit

--- passwordmanager/main.cpp
+++ passwordmanager/main.cpp
@@ -1,50 +1,117 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
 const int USERS = 1000;
 string UserList[USERS];
 int NoOfUser;
+string username;
+string password;
+string line;
+string file_content;
+vector<string> new_lines;
 class User {
 public:
-    User(string name, bool is_premium) : name_(name), is_premium_(is_premium) {}
+    User(string name, string password): name_(name), password_(password) {}
+     bool Exsistence(){
+       std::ifstream file;
+       string filename=GetName()+".txt";
+       file.open(filename.c_str());
+       if(file){
+        getline(file,line);
+        if(line==GetPassword()){
+            return true;
+        }
+       }
+       return false;
+     }
+    string GetName() const {
+        return name_;
 
-    string GetName() const { return name_; }
-    bool IsPremium() const { return is_premium_; }
-
+        }
+     string GetPassword() const {
+        return password_;
+     }
 private:
     string name_;
-    bool is_premium_;
+    string password_;
+
 };
 
 class Admin {
 public:
-    Admin();
+    Admin() {};
 
+    void RemoveUser(std::string path, std::string eraseLine) {
+    bool found=false;
+    int status;
+    string userfilename=eraseLine;
+    //std::string line;
+    std::ifstream fin;
+    fin.open(path.c_str());
 
-    void RemoveUser(int index) { users_.erase(users_.begin() + index); }
+   if (fin.fail()) {
+    std::cerr << "Failed to open file: " << path << std::endl;
 
-    void ViewUsers(string filename){
-        ifstream infile(filename.c_str());
-        if (!infile) {
-        cerr << "There was an error in displaying users" <<  endl;
+}else{
+        std::ofstream temp;
+    temp.open("temp.txt");
+
+    while (getline(fin, line)) {
+        // write all lines to temp other than the line marked for erasing
+       if (line.find(eraseLine) == std::string::npos){
+            temp << line << std::endl;
+       }else{
+       found=true;
+       }
+
+    }
+    temp.close();
+    fin.close();
+
+    // required conversion for remove and rename functions
+    const char * p = path.c_str();
+    remove(p);
+    rename("temp.txt", p);
+    if(found){
+        //Delete user file name
+        userfilename=userfilename+".txt";
+        status=std::remove(userfilename.c_str());
+
+            cout<<"User deleted sucessfully"<<endl;
+
 
     }else{
-        NoOfUser=0;
-       string line;
-       while (getline(infile, line) && NoOfUser < USERS) {
-        UserList[NoOfUser++] = line;
+       cout<<"User not found"<<endl;
     }
+}
 
-    for (int i = 0; i < NoOfUser; i++) {
-        cout << "Line " << i+1 << ": " << UserList[i] << endl;
-    }
-    }
+};
 
+    void ViewUsers(string filename) {
+        ifstream infile(filename.c_str());
+        //Check if the file exsist or not
+        if (!infile) {
+            cerr << "There was an error in displaying users" << endl;
+        } else {
+            NoOfUser = 0;
+            while (getline(infile, line) && NoOfUser < USERS) {
+                UserList[NoOfUser++] = line;
+            }
+
+            for (int i = 0; i < NoOfUser; i++) {
+                cout<< UserList[i] << endl;
+            }
+            cout<<"Number of users using the system "<<NoOfUser<<endl;
+            infile.close();
+        }
     };
-    int GetUserCount() const { return users_.size(); }
+
+
 
 private:
     string password_;
@@ -52,67 +119,74 @@ private:
 };
 
 int main() {
-     /*
-    User user1("Alice", true);
-    User user2("Bob", false);
+    int determine;
+    string adminPass = "";
+    const char* adminfile = "admin.txt";
+    string userfiles = "users.txt";
 
-    Admin admin("password");
-    admin.AddUser(user1);
-    admin.AddUser(user2);
+    while (true) {
+        cout << "Are you a regular user or an admin?? \n1. Regular user\n2. Admin\n3. Exit\n" << endl;
+        cin >> determine;
 
-    //cout << "Total number of users: " << admin.GetUserCount() << endl;
-*/
-// Variable declaration
-   int determine;
-   string adminPass="";
-   string file_content;
-   const char* adminfile="admin.txt";
-   string userfiles="users.txt";
-   while(true){
-    cout<< "Are you a regular user or an admin?? \n1. Regular user\n2. Admin\n3. Exit\n"<<endl;
-    cin>>determine;
-    if(determine==1){
-        //User Part
-        cout<<"You are a regular user"<<endl;
-    }else if(determine==2){
-         //Admin part
-              cout<<"Enter admin password \n press 3 to exit"<<endl;
-          cin>>adminPass;
-          ifstream file(adminfile);
-          //Check login status of admin
-          while (getline(file, file_content)) {
-    if (file_content == adminPass) {
-      cout << "Successfully Login as admin\n";
-               while (true){
+        if (determine == 1) {
+            // User Part
+            cout << "Are you a new or an exsisting user \n1. Existing user\n2. New user" << endl;
+            cin >> determine;
+            //Checking user input
+            if(determine==1){
+                cout<<"Enter your username or password"<<endl;
+                cin>>username;
+                cin>>password;
+                User user(username,password);
+                if(user.Exsistence()){
+                    cout<<"Good you exsist";
+                }else{
+                   cout<<"Sorry username or password not found";
+                   //delete user;
+                }
+            }
+           else if(determine==2){
+                cout<<"You are a new user"<<endl;
+            }else{
+               cout<<"Please give proper input"<<endl;
+            }
+        } else if (determine == 2) {
+            // Admin part
+            cout << "Enter admin password \n press 3 to exit" << endl;
+            cin >> adminPass;
+            ifstream file(adminfile);
+
+            // Check login status of admin
+            while (getline(file, file_content)) {
+                if (file_content == adminPass) {
+                    cout << "Successfully Login as admin\n";
                     Admin admin;
-                     cout<< "What do you want to do today?? \n1. View users user\n2. Delete users\n3. Logout\n"<<endl;
-                     cin>>determine;
-                     if(determine==1){
-                        //View Users
-                        admin.ViewUsers("users.txt");
-                     }
-                     if(determine==2){
-                        //Delete Users
-                     }
-                     if(determine==3){
-                        //Logout
-                        break;
-                     }
-                     }
 
-    }else{
-        cout<< "Please enter correct credentials"<<endl;
-    }
-         }
+                    while (true) {
+                        cout << "What do you want to do today?? \n1. View users user\n2. Delete users\n3. Logout\n" << endl;
+                        cin >> determine;
 
-         //cout<<"You are an admin"<<endl;
-    }else if(determine==3){
-    // Exit the program
-       cout<<"Thank you for using our program"<<endl;
-       break;
-    }else{
-       cout<<"Please give appropriate input"<<endl;
+                        if (determine == 1) {
+                            // View Users
+                            admin.ViewUsers("users.txt");
+                        } else if (determine == 2) {
+                            //Remove users
+                            admin.RemoveUser("users.txt","Rahee");
+                        } else if (determine == 3) {
+                            // Logout
+                            break;
+                        }
+                    }
+                } else {
+                    cout << "Please enter correct credentials" << endl;
+                }
+            }
+        } else if (determine == 3) {
+            // Exit the program
+            cout<<"Thank you for using our program"<<endl;
+            break;
+        }
+        username="";
+        password="";
     }
-   }
-    return 0;
 }
